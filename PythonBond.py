c9adf490eb6b8caccaa5c3e591dcb94d35b1c499
John <j.harvel31@gmail.com>
2017-02-27 17:15:41 +0100

    Creation of class bonds, try to do simulation

--- PythonBond.py
+++ PythonBond.py
@@ -1,7 +1,35 @@
-class Bonds(object):
+class Bond(object):
+    class Bond(object):
+        def __init__(self, rate, price, amount, maturity):
+            self.rate = rate
+            self.price = price
+            self.amount = amount
+            self.maturity = maturity
+            # Add your method here!
 
-    def __init__(self,name):
-        self.name=name
-    def printname(self):
-        short_term=Bonds("Short term bond")
-        print(self.name)
+        def description(self):
+            print(self.rate)
+            print(self.amount)
+            print(self.maturity)
+            print(self.price)
+
+        def simulation(self):
+            for i in range(102):
+                self.price = self.amount / (1 + self.rate) ** i
+            # self.amount+= 564
+            print(self.price)
+
+            # def getAlive(self):
+            # return self.alive
+
+    short_term = Bond(0.01, "", 1000, 2)
+
+    short_term.description()
+
+    long_term = Bond(0.03, "", 3000, 5)
+
+    long_term.description()
+
+    short_term.simulation()
+    long_term.simulation()
+
